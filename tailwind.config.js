bbc0f8227e5e32fe1054e600106b6b795c253cfb
Milan TURCANO <turcano.milan@sfr.fr>
2023-07-23 15:29:44 +0200

    J'essaye un peu tout

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'hsl(var(--color-primary) / <alpha-value>)',
+        white: 'hsl(var(--white) / <alpha-value>)',
+        light: 'hsl(var(--lightGrey) / <alpha-value>)',
+        medium: 'hsl(var(--MediumGrey) / <alpha-value>)',
+        darkBlue: 'hsl(var(--DarkBlue) / <alpha-value>)',
+        VeryDarkBlue: 'hsl(var(--VeryDarkBlue) / <alpha-value>)',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
